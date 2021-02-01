95f0759717ab652168ed4a1540505ec9a9786168
Blake Kostner <git@btkostner.io>
2021-01-31 20:00:02 -0700

    add postgres container to github ci tests

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,7 @@ config :neon, Neon.Repo,
   username: "postgres",
   password: "postgres",
   database: "neon_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("DATABASE_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :neon, NeonWeb.Endpoint,
